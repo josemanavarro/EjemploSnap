2b7ec501677a64e4a8e307475f896111f94ea181
josemanavarro <josemanuel.navarro@iesalandalus.org>
2024-04-10 18:32:33 +0200

    Primera subida

---
+++ src/main/java/org/iesalandalus/ejemplosnap/EjemploSnap.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.iesalandalus.ejemplosnap;
+
+/**
+ *
+ * @author usuario
+ */
+public class EjemploSnap {
+
+    public static void main(String[] args) {
+        EjemploSnapJFrame ventana = new EjemploSnapJFrame();
+        ventana.setVisible(true);
+    }
+}
